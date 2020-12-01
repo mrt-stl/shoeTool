5c2f3e078d58d6f9ade8728b1147514898c4215a
MartinGoesGitHub <kolitsch.martin@gmail.com>
2020-12-01 17:02:44 +0100

    create new chart with victory

--- src/Components/ChartNew.js
+++ src/Components/ChartNew.js
@@ -7,65 +7,44 @@ import {
     VictoryArea,
     VictoryLabel,
     VictoryLegend,
+    VictoryTooltip,
 } from "victory"
 import styled from "styled-components"
 
 const Container = styled.div`
-    height: 90vh;
-    margin: 0 auto;
-    padding-top: 50px;
+    width: 70%;
+    margin-top: 50px;
 `
 
 export default class ChartNew extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-          shoe1Data: [this.props.shoe1Data],
-          shoe2Data: [this.props.shoe2Data],
+            maxima: 10,
         }
     }
 
     componentWillReceiveProps(nextProps) {
         // You don't have to do this check first, but it can help prevent an unneeded render
-        if (nextProps.shoe1Data !== this.state.shoe1Data) {
+        if (
+            nextProps.shoe1Data !== this.props.shoe1Data ||
+            nextProps.shoe2Data !== this.props.shoe2Data
+        ) {
             this.setState({
-              shoe1Data: nextProps.shoe1Data,
-              data: this.processData([nextProps.shoe1Data, this.state.shoe2Data]),
-              maxima: this.getMaxima([nextProps.shoe1Data, this.state.shoe2Data])
+                shoe1Data: nextProps.shoe1Data,
+                shoe2Data: nextProps.shoe2Data,
+                data: this.processData([
+                    nextProps.shoe1Data,
+                    nextProps.shoe2Data,
+                ]),
             })
         }
-        if (nextProps.shoe2Data !== this.state.shoe2Data) {
-          this.setState({
-            shoe2Data: nextProps.shoe2Data,
-            data: this.processData([this.state.shoe1Data, nextProps.shoe2Data]),
-            maxima: this.getMaxima([this.state.shoe1Data, nextProps.shoe2Data])})
-        }
-    }
-
-    componentDidMount() {
-      this.setState({
-        data: this.processData([this.state.shoe1Data, this.state.shoe2Data]),
-        maxima: this.getMaxima([this.state.shoe1Data, this.state.shoe2Data])})
-    }
-
-    getMaxima(data) {
-        // transform data into array per poperty (example => Geschwindigkeit: [9, 6])
-        const groupedData = Object.keys(data[0]).reduce((memo, key) => {
-            memo[key] = data.map((d) => d[key])
-            return memo
-        }, {})
-        // making object with max values of every property
-        return Object.keys(groupedData).reduce((memo, key) => {
-            memo[key] = 10
-            return memo
-        }, {})
     }
 
     processData(data) {
-        const maxByGroup = this.getMaxima(data)
         const makeDataArray = (d) => {
             return Object.keys(d).map((key) => {
-                return { x: key, y: d[key] / maxByGroup[key] }
+                return { x: key, y: d[key] / 10 }
             })
         }
         return data.map((datum) => makeDataArray(datum))
@@ -80,14 +59,14 @@ export default class ChartNew extends React.Component {
                     domain={{ y: [0, 1] }}
                 >
                     <VictoryLegend
-                        x={300}
+                        x={250}
                         y={0}
                         title="Legende"
                         orientation="vertical"
                         gutter={25}
                         style={{
                             title: { fontSize: 9, fontFamily: "Rockwell" },
-                            labels: { fontFamily: "Rockwell", fontSize: 7 },
+                            labels: { fontFamily: "Rockwell", fontSize: 4 },
                         }}
                         data={[
                             {
@@ -107,38 +86,74 @@ export default class ChartNew extends React.Component {
                     <VictoryGroup
                         colorScale={["tomato", "gold"]}
                         style={{ data: { fillOpacity: 0.2, strokeWidth: 2 } }}
+                        labels={() => `KOMM SCHON!`}
+                        labelComponent={<VictoryTooltip />}
                     >
-                        {this.state.data ? this.state.data.map((data, i) => {
-                            return <VictoryArea key={i} data={data} />
-                        }) : null}
+                        {this.state.data
+                            ? this.state.data.map((data, i) => {
+                                  return <VictoryArea key={i} data={data} />
+                              })
+                            : null}
                     </VictoryGroup>
-                    {this.state.data ? Object.keys(this.state.maxima).map((key, i) => {
-                        return (
-                            <VictoryPolarAxis
-                                key={i}
-                                dependentAxis
-                                style={{
-                                    axisLabel: { padding: 30 },
-                                    axis: { stroke: "none" },
-                                    grid: {
-                                        stroke: "grey",
-                                        strokeWidth: 0.25,
-                                        opacity: 0.2,
-                                    },
-                                }}
-                                tickLabelComponent={
-                                    <VictoryLabel labelPlacement="vertical" />
-                                }
-                                labelPlacement="perpendicular"
-                                axisValue={i + 1}
-                                label={key}
-                                tickFormat={(t) =>
-                                    Math.ceil(t * this.state.maxima[key])
-                                }
-                                tickValues={[0.5, 1]}
-                            />
-                        )
-                    }) : null}
+                    {this.state.data
+                        ? this.state.data[0].map((key, i) => {
+                              return (
+                                  <VictoryPolarAxis
+                                      key={i}
+                                      dependentAxis
+                                      style={{
+                                          axisLabel: {
+                                              padding:
+                                                  i === 2 || i === 5 || i === 6
+                                                      ? 18
+                                                      : 30,
+                                              fontSize: 6,
+                                          },
+                                          axis: { stroke: "none" },
+                                          grid: {
+                                              stroke: "gray",
+                                              strokeWidth: 0.1,
+                                              opacity: 0.08,
+                                          },
+                                      }}
+                                      tickLabelComponent={
+                                          i === 1 ? (
+                                              <VictoryLabel
+                                                  labelPlacement="vertical"
+                                                  style={{
+                                                      fontSize: 4,
+                                                      fill: "gray",
+                                                      fontFamily: "Rockwell",
+                                                  }}
+                                              />
+                                          ) : (
+                                              <VictoryLabel
+                                                  style={{ display: "none" }}
+                                              />
+                                          )
+                                      }
+                                      labelPlacement="vertical"
+                                      axisValue={i + 1}
+                                      label={key.x}
+                                      tickFormat={(t) =>
+                                          Math.ceil(t * this.state.maxima)
+                                      }
+                                      tickValues={[
+                                          0.1,
+                                          0.2,
+                                          0.3,
+                                          0.4,
+                                          0.5,
+                                          0.6,
+                                          0.7,
+                                          0.8,
+                                          0.9,
+                                          1,
+                                      ]}
+                                  />
+                              )
+                          })
+                        : null}
                     <VictoryPolarAxis
                         labelPlacement="parallel"
                         tickFormat={() => ""}
