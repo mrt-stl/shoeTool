27d140df5f1940ee059ea05ac41376ce87157aee
MartinGoesGitHub <kolitsch.martin@gmail.com>
2020-12-23 15:49:22 +0100

    add redirect if user is already logged in

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import styled from "styled-components"
 import Title from "./elements/title"
 import { useAuth } from "../utils/use-auth"
-import { useLocation, useHistory } from "react-router-dom"
+import { useLocation, useHistory, Redirect } from "react-router-dom"
 
 const LoginContainer = styled.div`
     width: 100%;
@@ -33,63 +33,64 @@ const Button = styled.button`
 `
 
 export default function Login() {
-
     const [userMail, setUserMail] = useState("")
     const [userPassword, setUserPassword] = useState("")
 
-     const validateFormFields = () => {
-        return (
-            userPassword !== "" && userMail !== ""
-        )
+    const validateFormFields = () => {
+        return userPassword !== "" && userMail !== ""
     }
 
     const auth = useAuth()
     const history = useHistory()
     const location = useLocation()
 
-    let { from } = location.state || { from: { pathname: "/" } };
+    let { from } = location.state || { from: { pathname: "/" } }
 
     const handleSubmit = () => {
         auth.signin(userMail, userPassword, () => {
             history.replace(from)
         })
-
     }
 
-
-        return (
-            <LoginContainer>
-                <Title title="Login" />
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <Label htmlFor="userName">E-Mail</Label>
-                        <Input
-                            value={userMail}
-                            type="text"
-                            onChange={e => setUserMail(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <Label htmlFor="userPassword">Passwort</Label>
-                        <Input
-                            value={userPassword}
-                            type="password"
-                            onChange={e => setUserPassword(e.target.value)}
-                            data-test="password"
-                        />
-                    </div>
-                    <div>
-                        <Button
-                            block
-                            bsSize="large"
-                            disabled={!validateFormFields()}
-                            type="submit"
-                        >
-                            Login
-                        </Button>
-                    </div>
-                </form>
-            </LoginContainer>
-        )
-    
+    return auth.user ? (
+        <Redirect
+            to={{
+                pathname: "/admin",
+                state: { from: location },
+            }}
+        />
+    ) : (
+        <LoginContainer>
+            <Title title="Login" />
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <Label htmlFor="userName">E-Mail</Label>
+                    <Input
+                        value={userMail}
+                        type="text"
+                        onChange={(e) => setUserMail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <Label htmlFor="userPassword">Passwort</Label>
+                    <Input
+                        value={userPassword}
+                        type="password"
+                        onChange={(e) => setUserPassword(e.target.value)}
+                        data-test="password"
+                    />
+                </div>
+                <div>
+                    <Button
+                        block
+                        bsSize="large"
+                        disabled={!validateFormFields()}
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+                </div>
+            </form>
+        </LoginContainer>
+    )
 }
